c67f4c47ac24435eca1077ded983c06a6546c574
Cristian Zambrano <cristian_uteq@hotmail.com>
2025-12-27 12:47:32 -0500

    Changes on os.getenv

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "tu_password")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "123456")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME", "tienda")
